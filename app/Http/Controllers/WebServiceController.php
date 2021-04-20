e7283a78c6919a05f606347b619bb9642683f952
jhonaikerf <jhonaikerfarias@gmail.com>
2021-04-20 15:21:29 -0400

    fix styles

--- app/Http/Controllers/WebServiceController.php
+++ app/Http/Controllers/WebServiceController.php
@@ -47,14 +47,14 @@ class WebServiceController extends Controller
         } else {
             $saldoVigencia = 0;
         }
-        $data = (object)['saldo' => $saldo, 'status' => $vigencia['status'], 'saldo_vigencia' => $saldoVigencia ];
+        $data = (object)['saldo' => $saldo, 'status' => $vigencia['status'], 'saldo_vigencia' => $saldoVigencia];
         $this->saldoRepository->deleteAndInsert($data);
-        $data = (object)['saldo' => number_format((float)$saldo, 2), 'status' => $vigencia['status'], 'saldo_vigencia' => $saldoVigencia ];
+        $data = (object)['saldo' => number_format((float)$saldo, 2), 'status' => $vigencia['status'], 'saldo_vigencia' => $saldoVigencia];
         return response()->json([
-      'cache' => true,
-      'success' => true,
-      'data' => $data,
-    ]);
+            'cache' => true,
+            'success' => true,
+            'data' => $data,
+        ]);
     }
 
     public function getUnpaidInvoices(Request $request)
@@ -80,9 +80,9 @@ class WebServiceController extends Controller
     {
         $data = $this->soapService->getReportedPayments();
         return response()->json([
-      'success' => true,
-      'data' => $data,
-    ]);
+            'success' => true,
+            'data' => $data,
+        ]);
         ;
     }
 
@@ -116,54 +116,20 @@ class WebServiceController extends Controller
         $user = auth()->user()->group_id;
         $data = \DB::connection('sqlsrv_backoffice')->statement(
             'exec sp_PortalProcesarPagoFactura ?,?,?,?,?',
-            array($user,$request['invoice'], $request['amount'],$request['order'], $request['dTasa'])
+            array($user, $request['invoice'], $request['amount'], $request['order'], $request['dTasa'])
         );
 
         if (!$data) {
             return response()->json([
-        'success' => false,
-        'message' => 'Error de registro'
-      ])->setStatusCode(400);
+                'success' => false,
+                'message' => 'Error de registro'
+            ])->setStatusCode(400);
         }
 
         return response()->json([
-      'success' => true,
-      'message' => $data
-    ]);
-        // $data = \DB::connection('sqlsrv_backoffice')->select('
-    // EXEC backoffice.dbo.sp_PortalProcesarPagoFactura group_id,invoices,amount,paymentNumber',
-    // array("'3453453'",'"34534"', 120,'345345'));
-
-    // $db = DB::connection('sqlsrv_backoffice')->getPdo();
-    // $db->setAttribute(PDOConnection::ATTR_ERRMODE, PDOConnection::ERRMODE_EXCEPTION);
-    // $a = 'assas';
-    // $amount = 120;
-    // $queryResult = $db->prepare('exec backoffice.dbo.sp_PortalProcesarPagoFactura group_id,invoices,amount,paymentNumber');
-    // $queryResult->bindParam(1, $a, PDOConnection::PARAM_STR);
-    // $queryResult->bindParam(2, $a, PDOConnection::PARAM_STR);
-    // $queryResult->bindParam(3, $amount, PDOConnection::PARAM_INT);
-    // $queryResult->bindParam(4, $a, PDOConnection::PARAM_STR);
-    // $queryResult->execute();
-    // $result_set = $queryResult->fetchAll(PDOConnection::FETCH_ASSOC);
-    // $queryResult->closeCursor();
-    // return $result_set;
-
-//     $pdo = \DB::connection('sqlsrv_backoffice')->getPdo();
-//     $sql = 'EXEC backoffice.dbo.sp_PortalProcesarPagoFactura group_id,invoices,amount,paymentNumber';
-
-//     $stmt = $pdo->query($sql);
-//     $stmt->bindParam(1, 'asdasd', PDO::PARAM_STR, 4000);
-//     $stmt->bindParam(2, 'asdasd', PDO::PARAM_STR, 4000);
-//     $stmt->bindParam(3, 120);
-//     $stmt->bindParam(4, 'asdasd', PDO::PARAM_STR, 4000);
-//     do {
-//       $rows = $stmt->fetchAll(\PDO::FETCH_NUM); // Keys will be start from zero , one, two
-//       $rows = $stmt->fetchAll(\PDO::FETCH_ASSOC); // Column names will be assigned for each value
-
-//       if ($rows) {
-//         $sheetData[] = $rows;
-//       }
-// } while ($stmt->nextRowset());
+            'success' => true,
+            'message' => $data
+        ]);
     }
 
     public function setManualInvoicePayment(Request $request)
@@ -171,28 +137,28 @@ class WebServiceController extends Controller
         $user = auth()->user()->group_id;
         $data = \DB::connection('sqlsrv_backoffice')->statement(
             'exec sp_PortalPagoFacturaManual ?,?,?,?,?',
-            array($request['share'],$request['numFactura'], $request['idPago'], $request['fechaPago'], 'MANUAL')
+            array($request['share'], $request['numFactura'], $request['idPago'], $request['fechaPago'], 'MANUAL')
         );
 
         if (!$data) {
             return response()->json([
-        'success' => false,
-        'message' => 'Error de registro'
-      ])->setStatusCode(400);
+                'success' => false,
+                'message' => 'Error de registro'
+            ])->setStatusCode(400);
         }
 
         return response()->json([
-      'success' => true,
-      'message' => $data
-    ]);
+            'success' => true,
+            'message' => $data
+        ]);
     }
 
     public function getTasaDelDia()
     {
         $data = $this->soapService->getTasaDelDia();
         return response()->json([
-      'success' => true,
-      'data' => $data
-    ]);
+            'success' => true,
+            'data' => $data
+        ]);
     }
 }
